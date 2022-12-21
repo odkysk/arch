6c8958668d875478f87810ee78b46d3af50869e8
odkysk <odkysk@gmail.com>
2022-12-21 09:49:40 +0900

    make relation position works

--- src/components/Relation.tsx
+++ src/components/Relation.tsx
@@ -1,26 +1,36 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { Position } from "../models/Position";
 interface Props {
   start: Position;
   end: Position;
 }
 export const Relation = ({ start, end }: Props) => {
+  const viewWidth = 1200;
+  const viewHeight = 800;
+  const startRelative: Position = {
+    x: start.x + viewWidth / 2,
+    y: start.y + viewHeight / 2,
+  };
+  const endRelative: Position = {
+    x: end.x + viewWidth / 2,
+    y: end.y + viewHeight / 2,
+  };
   return (
-    <div>
-      <p>
-        <p>
-          {start.x}, {start.y}
-        </p>
-        <p>
-          {end.x}, {end.y}
-        </p>
-      </p>
-      <svg id="Relation" width="400" height="300" viewBox="0 0 400 300">
+    <div
+      css={css`
+        position: absolute;
+        left: -600px;
+        top: -400px;
+      `}
+    >
+      <svg id="Relation" width="1200" height="800" viewBox="0 0 1200 800">
         <line
-          x1={start.x}
-          y1={start.y}
-          x2={end.x}
-          y2={end.y}
-          stroke="black"
+          x1={startRelative.x + 30}
+          y1={startRelative.y + 15}
+          x2={endRelative.x + 30}
+          y2={endRelative.y + 15}
+          stroke="rgba(255,0,0,0.5)"
           strokeWidth="2"
         />
       </svg>
