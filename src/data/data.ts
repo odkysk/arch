59ddf59e90e6c6422ccb6635ad6db6a1ba9ecfae
odkysk <odkysk@gmail.com>
2023-01-07 23:17:10 +0900

    randomize initial relation color

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,9 +1,17 @@
+import _ from "lodash";
+import { canvasColors } from "../models/Color";
 import { Data } from "../models/Data";
 
+const firstRelationColor = _.sample(canvasColors) || "blue";
 export const data: Data = {
   members: [],
   relations: [
-    { id: "0", name: "new relation", color: "blue", shape: "straight" },
+    {
+      id: "0",
+      name: "new relation",
+      color: firstRelationColor,
+      shape: "straight",
+    },
   ],
   connections: [],
   views: [{ id: "0", name: "All" }],
