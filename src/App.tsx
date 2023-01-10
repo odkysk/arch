341a70b94ddd3901ad4b895bb8d3d7977fa3cc94
odkysk <odkysk@gmail.com>
2023-01-10 22:07:35 +0900

    Escape key to selection mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { KeyboardEvent } from "react";
+import { KeyboardEvent, useContext } from "react";
 import "reseter.css";
 import { Canvas } from "./components/arrangements/Canvas";
 import { Cursor } from "./components/arrangements/Cursor";
 import { Panels } from "./components/arrangements/Panels";
+import { ToolContext } from "./contexts/toolContext";
 import { systemColors } from "./styles/colors";
 import "./styles/global.css";
 
 export default function App() {
+  const toolContext = useContext(ToolContext);
   const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
     if (event.repeat) return;
-    console.log(event.key);
     if (event.key === "Escape") {
+      toolContext.setSelectionMode();
     }
   };
   return (
-    <main css={main} onKeyDown={handleKeyDown}>
+    <main css={main} tabIndex={0} onKeyDown={handleKeyDown}>
       <Canvas />
       <Panels />
       <Cursor />
